da5913f6eff38671816a792f0d7a2d059f407ca4
Krishna Vamsi Pinninti <krishpinninti789@gmail.com>
2025-05-27 16:57:38 +0530

    Class and Inheritance

---
+++ ClassExample.java
@@ -0,0 +1,23 @@
+
+class ExampleClass1{
+    public void show(){
+        System.out.print("In Class 1 method");
+    }
+}
+
+
+
+
+
+
+
+
+public class ClassExample {
+    public static void main(String[] args) {
+        System.out.println("In main class");
+        ExampleClass1 ex = new ExampleClass1();
+        ex.show();
+    }
+
+    
+}
---
+++ Extends.java
@@ -0,0 +1,26 @@
+class  Parent{
+    public void show(){
+        System.out.println("This is parent class method");
+    }
+}
+
+class Child extends Parent {
+    public void display(){
+        System.out.println("This is child method");
+
+    }
+     
+}
+
+
+
+
+public class Extends {
+    public static void main(String[] args)throws Exception {
+        Child ch = new Child();
+        ch.show();
+        ch.display();
+        
+    }
+    
+}
